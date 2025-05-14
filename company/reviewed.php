8575701323669dbdc196df8b472c156d12b7ef16
Amadare <amxdxre@gmail.com>
2025-05-14 18:13:54 +0300

    Задано другое значение.

--- company/reviewed.php
+++ company/reviewed.php
@@ -1,7 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("test", "Тестовое значение");
-$APPLICATION->SetPageProperty("title", "Отзывы");
+$APPLICATION->SetPageProperty("title", "Заголовок окна браузера");
 $APPLICATION->SetPageProperty("keywords", "Отзывы, компания, мебель");
 $APPLICATION->SetPageProperty("description", "Отзывы о компании");
 $APPLICATION->SetTitle("Отзывы");
