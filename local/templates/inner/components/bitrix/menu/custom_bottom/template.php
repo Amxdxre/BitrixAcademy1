ddc5cd7dcc54844932fb121f5ea4b69f585b48c4
Amadare <amxdxre@gmail.com>
2025-11-27 17:35:20 +0300

    Шаблон нижнего меню О Магазине адаптирован под верстку из материалов урока и подключен в шаблоне внутренних страниц.

--- local/templates/inner/components/bitrix/menu/custom_bottom/template.php
+++ local/templates/inner/components/bitrix/menu/custom_bottom/template.php
@@ -1,15 +1,16 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?if (!empty($arResult)):?>
-<ul class="left-menu">
-
+<div class="ft_about">
+	<h4><?=GetMessage('ABOUT_STORE')?></h4>
+<ul>
 <?
 foreach($arResult as $arItem):
 	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
 		continue;
 ?>
 	<?if($arItem["SELECTED"]):?>
-		<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
+		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
 	<?else:?>
 		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
 	<?endif?>
@@ -17,4 +18,5 @@ foreach($arResult as $arItem):
 <?endforeach?>
 
 </ul>
+</div>
 <?endif?>
