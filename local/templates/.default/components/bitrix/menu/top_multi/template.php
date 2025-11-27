a53669a4b4f52cabee263089bba59e570e569cc9
Amadare <amxdxre@gmail.com>
2025-11-27 17:28:45 +0300

    Синхронизация с сервером.

--- local/templates/.default/components/bitrix/menu/top_multi/template.php
+++ local/templates/.default/components/bitrix/menu/top_multi/template.php
@@ -1,11 +1,11 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?if (!empty($arResult)):?>
-	<div class="nv_topnav">
-<ul>
+<div class="nv_topnav">
+	<ul>
 
-<?
-$previousLevel = 0;
+	<?
+	$previousLevel = 0;
 foreach($arResult as $arItem):?>
 
 	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
@@ -14,20 +14,20 @@ foreach($arResult as $arItem):?>
 
 	<?if ($arItem["IS_PARENT"]):?>
 
-		<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-			<li><a href="<?=$arItem["LINK"]?>"<?if (isset($arItem['PARAMS']['IMG'])):?> class="menu-img-fon" style="background-image: url(<?=$arItem['PARAMS']['IMG']?>);"<?endif?><span><?=$arItem["TEXT"]?></span></a>
-				<ul>
-		<?else:?>
-			<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
-				<ul>
-		<?endif?>
+	<?if ($arItem["DEPTH_LEVEL"] == 1):?>
+	<li><a href="<?=$arItem["LINK"]?>" <?if (isset($arItem['PARAMS']['IMG'])):?>class="menu-img-fon" style="background-image: url(<?=$arItem['PARAMS']['IMG']?>)" <?endif;?>><span><?=$arItem["TEXT"]?></span></a>
+	<ul>
+	<?else:?>
+	<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+	<ul>
+	<?endif?>
 
 	<?else:?>
 
 		<?if ($arItem["PERMISSION"] > "D"):?>
 
 			<?if ($arItem["DEPTH_LEVEL"] == 1):?>
-				<li><a href="<?=$arItem["LINK"]?>"<?if (isset($arItem['PARAMS']['IMG'])):?> class="menu-img-fon" style="background-image: url(<?=$arItem['PARAMS']['IMG']?>);"<?endif?>><span><?=$arItem["TEXT"]?></span></a></li>
+				<li><a href="<?=$arItem["LINK"]?>" <?if (isset($arItem['PARAMS']['IMG'])):?>class="menu-img-fon" style="background-image: url(<?=$arItem['PARAMS']['IMG']?>)" <?endif;?>><span><?=$arItem["TEXT"]?></span></a></li>
 			<?else:?>
 				<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
 			<?endif?>
@@ -40,10 +40,10 @@ foreach($arResult as $arItem):?>
 
 <?endforeach?>
 
-<?if ($previousLevel > 1)://close last item tags?>
-	<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
-<?endif?>
-                    <div class="clearboth"></div>
-</ul>
-    </div>
+	<?if ($previousLevel > 1)://close last item tags?>
+		<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
+	<?endif?>
+		<div class="clearboth"></div>
+	</ul>
+</div>
 <?endif?>
