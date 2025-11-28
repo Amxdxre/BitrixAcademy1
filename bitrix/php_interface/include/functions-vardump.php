96d13d95936dfaad1b07faadebad5408ba6d1351
Amadare <amxdxre@gmail.com>
2025-11-28 15:14:19 +0300

    Доработка функции отладки на вывод только администраторам.

--- bitrix/php_interface/include/functions-vardump.php
+++ bitrix/php_interface/include/functions-vardump.php
@@ -2,7 +2,7 @@
 function vardump($var, $die = false, $all = false)
 {
 	global $USER;
-	if( ($USER->GetID() == 1) || ($all == true))
+	if( ($USER->isAdmin()) || ($all == true))
 	{
 		?>
 		<font style="text-align: left; font-size: 15px"><pre><?var_dump($var)?></pre></font><br>
