078ff0b466c71584b8ad3aa3b5ee43744ebc827a
Amadare <amxdxre@gmail.com>
2025-11-28 19:39:56 +0300

    Шаблон компонента рандомных отзывов адаптирован под шаблон из материалов урока и подключен в footer.php.

--- local/templates/inner/footer.php
+++ local/templates/inner/footer.php
@@ -40,14 +40,64 @@ IncludeTemplateLangFile(__FILE__);
         <h5><a href="">Мебельная полка всего за 560 Р</a></h5>
         <a href="" class="sb_action_more">Подробнее &rarr;</a>
     </div>
-    <div class="sb_reviewed">
-        <img src="/local/templates/.default/content/8.png" class="sb_rw_avatar" alt=""/>
-        <span class="sb_rw_name">Сергей Антонов</span>
-        <span class="sb_rw_job">Руководитель финансового отдела “Банк+”</span>
-        <p>“Покупал офисные стулья и столы, остался очень доволен! Низкие цены, быстрая доставка, обслуживание на высоте! Спасибо!”</p>
-        <div class="clearboth"></div>
-        <div class="sb_rw_arrow"></div>
-    </div>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "random_review",
+        Array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "CHECK_DATES" => "Y",
+            "COMPONENT_TEMPLATE" => ".default",
+            "DETAIL_URL" => "",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "DISPLAY_TOP_PAGER" => "N",
+            "FIELD_CODE" => [0=>"",1=>"",],
+            "FILTER_NAME" => "",
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+            "IBLOCK_ID" => "7",
+            "IBLOCK_TYPE" => "content",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+            "INCLUDE_SUBSECTIONS" => "Y",
+            "MESSAGE_404" => "",
+            "NEWS_COUNT" => "1",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => ".default",
+            "PAGER_TITLE" => "Новости",
+            "PARENT_SECTION" => "",
+            "PARENT_SECTION_CODE" => "",
+            "PREVIEW_TRUNCATE_LEN" => "",
+            "PROPERTY_CODE" => [0=>"POSITION",1=>"COMPANY_NAME",2=>"",],
+            "SET_BROWSER_TITLE" => "N",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "N",
+            "SET_META_KEYWORDS" => "N",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "N",
+            "SHOW_404" => "N",
+            "SORT_BY1" => "rand",
+            "SORT_BY2" => "SORT",
+            "SORT_ORDER1" => "",
+            "SORT_ORDER2" => "ASC",
+            "STRICT_SECTION_CHECK" => "N"
+        )
+    );?>
 </div>
 <div class="clearboth"></div>
 </div>
