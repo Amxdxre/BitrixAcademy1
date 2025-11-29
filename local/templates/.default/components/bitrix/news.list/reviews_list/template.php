b842a81eb30592b27da156dbf3cdbf3845c71318
Amadare <amxdxre@gmail.com>
2025-11-29 19:26:52 +0300

    Добавлена поддержка "Эрмитаж".

--- local/templates/.default/components/bitrix/news.list/reviews_list/template.php
+++ local/templates/.default/components/bitrix/news.list/reviews_list/template.php
@@ -14,6 +14,11 @@ $this->setFrameMode(true);
 ?>
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+<div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 <div class="review-block">
 	<div class="review-text">
 		<div class="review-block-title"><span class="review-block-name"><a href="#"><?=$arItem["NAME"]?></a></span><span class="review-block-description"><?=$arItem["PROPERTIES"]["POSITION"]["VALUE"]?> <?=$arItem["PROPERTIES"]["COMPANY_NAME"]["VALUE"]?></span></div>
@@ -22,5 +27,6 @@ $this->setFrameMode(true);
 		</div>
 	</div>
 	<div class="review-img-wrap"><a href="#"><img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="img"></a></div>
+</div>
 </div>
 <?endforeach;?>
