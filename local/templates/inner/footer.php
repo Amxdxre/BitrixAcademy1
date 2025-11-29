cacf4fce85a83a0d29c47c8fda929a65792b1f07
Amadare <amxdxre@gmail.com>
2025-11-29 15:18:24 +0300

    Шаблон компонента список новостей для последней акции адаптирован под шаблон из материлов урока и подключен в footer.php.

--- local/templates/inner/footer.php
+++ local/templates/inner/footer.php
@@ -34,12 +34,64 @@ IncludeTemplateLangFile(__FILE__);
             "EDIT_TEMPLATE" => ""
         )
     );?>
-    <div class="sb_action">
-        <a href=""><img src="/local/templates/.default/content/11.png" alt=""/></a>
-        <h4>Акция</h4>
-        <h5><a href="">Мебельная полка всего за 560 Р</a></h5>
-        <a href="" class="sb_action_more">Подробнее &rarr;</a>
-    </div>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "events_block",
+        Array(
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "ADD_SECTIONS_CHAIN" => "N",
+            "AJAX_MODE" => "N",
+            "AJAX_OPTION_ADDITIONAL" => "",
+            "AJAX_OPTION_HISTORY" => "N",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "Y",
+            "CACHE_FILTER" => "N",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_TIME" => "36000000",
+            "CACHE_TYPE" => "A",
+            "CHECK_DATES" => "Y",
+            "COMPONENT_TEMPLATE" => "events_block",
+            "DETAIL_URL" => "",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "DISPLAY_TOP_PAGER" => "N",
+            "FIELD_CODE" => [0=>"",1=>"",],
+            "FILTER_NAME" => "",
+            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+            "IBLOCK_ID" => "8",
+            "IBLOCK_TYPE" => "content",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+            "INCLUDE_SUBSECTIONS" => "Y",
+            "MESSAGE_404" => "",
+            "NEWS_COUNT" => "1",
+            "PAGER_BASE_LINK_ENABLE" => "N",
+            "PAGER_DESC_NUMBERING" => "N",
+            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+            "PAGER_SHOW_ALL" => "N",
+            "PAGER_SHOW_ALWAYS" => "N",
+            "PAGER_TEMPLATE" => ".default",
+            "PAGER_TITLE" => "Новости",
+            "PARENT_SECTION" => "",
+            "PARENT_SECTION_CODE" => "",
+            "PREVIEW_TRUNCATE_LEN" => "",
+            "PROPERTY_CODE" => [0=>"PRICE",1=>"",],
+            "SET_BROWSER_TITLE" => "Y",
+            "SET_LAST_MODIFIED" => "N",
+            "SET_META_DESCRIPTION" => "Y",
+            "SET_META_KEYWORDS" => "Y",
+            "SET_STATUS_404" => "N",
+            "SET_TITLE" => "Y",
+            "SHOW_404" => "N",
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_BY2" => "",
+            "SORT_ORDER1" => "DESC",
+            "SORT_ORDER2" => "",
+            "STRICT_SECTION_CHECK" => "N"
+        )
+    );?>
     <?$APPLICATION->IncludeComponent(
         "bitrix:news.list",
         "random_review",
